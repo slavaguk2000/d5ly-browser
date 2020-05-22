8be6f8b21c1cbfdc26c9537392bbabaee68d8c20
Viacheslav Guk <slavaguk2000@gmail.com>
2020-05-22 07:26:54 +0300

    clear repo

--- public/code/zlib/archiver.h
+++
@@ -1,7 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS
-#define MEMORY_ERROR -1
-namespace zlib{
-	int my_compress(int pointer, int buffer, int source_size, int level);
-	int my_decompress(int compressedBuffer, int compressedSize, int uncompressedBuffer, int uncompressedSize);
-	int gzipCompress(int sourcePointer, int gzipPointer, int size);
-}
