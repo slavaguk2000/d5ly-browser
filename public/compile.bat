7d2d23aea5fd463dced13635088117d7acff4830
Viacheslav Guk <slavaguk2000@gmail.com>
2020-06-21 06:24:30 +0300

    beauty compile.bat

--- public/compile.bat
+++ public/compile.bat
@@ -1,4 +1,25 @@
 cls
+
+@echo off
+
+echo.
+echo. 
+echo.
+echo. 
+echo      \\\\\\\          \\\\\\       \\          \\     \\\\\\\\      \\\\\\    \\           \\\\\\\\ 
+echo    \\       \\      \\      \\     \\\\      \\\\     \\      \\      \\      \\           \\ 
+echo    \\              \\        \\    \\  \\  \\  \\     \\       \\     \\      \\           \\   
+echo    \\              \\        \\    \\    \\    \\     \\      \\      \\      \\           \\\\\ 
+echo    \\              \\        \\    \\          \\     \\\\\\\\        \\      \\           \\ 
+echo    \\        \\     \\      \\     \\          \\     \\              \\      \\           \\ 
+echo      \\\\\\\\         \\\\\\       \\          \\     \\            \\\\\\    \\\\\\\\\\   \\\\\\\\\
+echo.
+echo. 
+echo.
+echo.
+
+@echo on
+
 emcc --bind -O3 -s ALLOW_MEMORY_GROWTH=1 -s ASSERTIONS=1 ^
 -o d5ly.js code/main.cpp ^
 code/core/d5ly.c code/core/lib/deflate_compress.c code/core/lib/deflate_decompress.c code/core/lib/aligned_malloc.c code/core/lib/gzip_compress.c code/core/lib/crc32.c ^
